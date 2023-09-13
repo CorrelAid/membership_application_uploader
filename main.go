ad7138a135c76f69c7a1d8f6d27f5bfb928f7fed
jstet <mail@jstet.net>
2023-09-13 14:19:14 -0700

    close #10

--- main.go
+++ main.go
@@ -15,12 +15,13 @@ import (
 	"time"
 
 	"github.com/CorrelAid/membership_application_uploader/inits"
+	"github.com/CorrelAid/membership_application_uploader/middleware"
 	"github.com/CorrelAid/membership_application_uploader/models"
+	"github.com/CorrelAid/membership_application_uploader/operations"
 	"github.com/CorrelAid/membership_application_uploader/validators"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	"golang.org/x/time/rate"
 )
 
 var MaxSize int
@@ -48,32 +49,21 @@ func main() {
 
 	router := gin.Default()
 
-	rateLimiter := rate.NewLimiter(rate.Every(time.Minute), 10)
+	// Rate Limiting
+	router.Use(middleware.RateLimitMiddleware())
 
-	// Configure IP-based rate limiting middleware
-	router.Use(func(c *gin.Context) {
-		if !rateLimiter.Allow() {
-			c.JSON(http.StatusTooManyRequests, gin.H{
-				"message": "Too many requests. Please try again later.",
-			})
-			c.Abort()
-			return
-		}
-		c.Next()
-	})
-
-	// Configure CORS middleware
+	// CORS
 	config := cors.DefaultConfig()
 	config.AllowOrigins = []string{"http://correlaid.org", "http://localhost"}
 	config.AllowMethods = []string{"POST"}
 	router.Use(cors.New(config))
 
 	// Set a lower memory limit for multipart forms (default is 32 MiB)
-	router.MaxMultipartMemory = 8 << 20 // 8 MiB
+	router.MaxMultipartMemory = 3 << 20 // 3 MiB
 	router.ForwardedByClientIP = true
 	router.SetTrustedProxies(nil)
 	router.POST("/", handle)
-	// Listen on the correct port
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080" // Default to port 8080 if PORT environment variable is not set
@@ -142,7 +132,10 @@ func handle(c *gin.Context) {
 func uploadFileToNextcloud(processedFormData models.ProcessedFormData, currentTime string) error {
 	client := &http.Client{}
 
-	filename := fmt.Sprintf("%s_%s", processName(processedFormData.Name), currentTime)
+	lowercase := strings.ToLower(processedFormData.Name)
+	result := strings.ReplaceAll(lowercase, " ", "_")
+
+	filename := fmt.Sprintf("%s_%s", result, currentTime)
 
 	req, err := http.NewRequest(http.MethodPut, "https://cloud.correlaid.org/remote.php/dav/files/bot@correlaid.org/Mitgliedsanträge/"+filename+".pdf", bytes.NewReader(processedFormData.FileContent))
 	if err != nil {
@@ -169,37 +162,6 @@ func uploadFileToNextcloud(processedFormData models.ProcessedFormData, currentTi
 	if err != nil {
 		return err
 	}
-	insertMember(processedFormData, currentTime)
+	operations.InsertMember(processedFormData, currentTime)
 	return nil
-}
-func insertMember(processedFormData models.ProcessedFormData, currentTime string) error {
-	newMember := &models.Member{
-		Email:  processedFormData.Email,
-		Name:   processedFormData.Name,
-		Time:   currentTime,
-		Expiry: time.Now().Add(24 * 14 * time.Hour).Format(time.RFC1123),
-	}
-
-	txn := inits.DB.Txn(true)
-	defer txn.Abort()
-
-	if err := txn.Insert("member", newMember); err != nil {
-		return err
-	}
-
-	txn.Commit()
-
-	log.Printf("Inserted member: email=%s", newMember.Email)
-
-	return nil
-}
-
-func processName(input string) string {
-	// Convert the string to lowercase
-	lowercase := strings.ToLower(input)
-
-	// Replace spaces with underscores
-	result := strings.ReplaceAll(lowercase, " ", "_")
-
-	return result
 }
